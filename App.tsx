8fa45f4b739cb3ef71f7dc88303c03ddae53ba09
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-27 12:28:42 +1000

    Start create password generator app.

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
+const styles = StyleSheet.create({});
