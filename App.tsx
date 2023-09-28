c6914dc6e2e91cfbc9fff617c58b1b4498aa568c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-28 11:42:27 +1000

    Create password schema and stats.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+// form validation
+import * as Yup from 'yup';
+
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be min of 4 numbers')
+    .max(12, 'Should be Max of 12 numbers')
+    .required('Length is required'),
+});
+
 const App = () => {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {};
+  const createPassword = (characters: string, passwordLength: number) => {};
+  const resetPasswordState = () => {};
+
   return (
     <SafeAreaView>
       <View>
